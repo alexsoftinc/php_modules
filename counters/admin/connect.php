c36a71799a996e6889c08ff20a52a1f2b7389ee9
Alexander Marenin <alexsoftinc@users.noreply.github.com>
2018-01-26 03:29:56 +0500

    Create connect.php

---
+++ counters/admin/connect.php
@@ -0,0 +1,7 @@
+<?php 
+$db_host = 'localhost';
+$db_user = '#####';
+$db_pswd = '#####';
+$db_name = '#####';
+
+$db = mysqli_connect($db_host, $db_user, $db_pswd, $db_name) or die('Подключение не может быть установлено');
