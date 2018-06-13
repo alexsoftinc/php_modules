4d0179f8b07c1e0370a80c074e043bc0bdc99395
Alexander Marenin <alexsoftinc@users.noreply.github.com>
2018-06-13 21:15:25 +0500

    Create generate.php

---
+++ unick_simbol/generate.php
@@ -0,0 +1,138 @@
+function num2alpha($n) {
+    $r = '';
+    for ($i = 1; $n >= 0 && $i < 10; $i++) {
+        $r = chr(0x41 + ($n % pow(26, $i) / pow(26, $i - 1))) . $r;
+        $n -= pow(26, $i);
+    }
+    return $r;
+} // Возвращает буквы при использовании 
+// Function my first Internet Shop 
+
+/**
+ * Возвращает сумму прописью
+ * @author AlexSoft
+ * @uses morph(...)
+ */
+function num2str($num) {
+    $nul='ноль';
+    $ten=array(
+        array('','один','два','три','четыре','пять','шесть','семь', 'восемь','девять'),
+        array('','одна','две','три','четыре','пять','шесть','семь', 'восемь','девять'),
+    );
+    $a20=array('десять','одиннадцать','двенадцать','тринадцать','четырнадцать' ,'пятнадцать','шестнадцать','семнадцать','восемнадцать','девятнадцать');
+    $tens=array(2=>'двадцать','тридцать','сорок','пятьдесят','шестьдесят','семьдесят' ,'восемьдесят','девяносто');
+    $hundred=array('','сто','двести','триста','четыреста','пятьсот','шестьсот', 'семьсот','восемьсот','девятьсот');
+    $unit=array( // Units
+        array('копейка' ,'копейки' ,'копеек',	 1),
+        array('рубль'   ,'рубля'   ,'рублей'    ,0),
+        array('тысяча'  ,'тысячи'  ,'тысяч'     ,1),
+        array('миллион' ,'миллиона','миллионов' ,0),
+        array('миллиард','милиарда','миллиардов',0),
+    );
+    //
+    list($rub,$kop) = explode('.',sprintf("%015.2f", floatval($num)));
+    $out = array();
+    if (intval($rub)>0) {
+        foreach(str_split($rub,3) as $uk=>$v) { // by 3 symbols
+            if (!intval($v)) continue;
+            $uk = sizeof($unit)-$uk-1; // unit key
+            $gender = $unit[$uk][3];
+            list($i1,$i2,$i3) = array_map('intval',str_split($v,1));
+            // mega-logic
+            $out[] = $hundred[$i1]; # 1xx-9xx
+            if ($i2>1) $out[]= $tens[$i2].' '.$ten[$gender][$i3]; # 20-99
+            else $out[]= $i2>0 ? $a20[$i3] : $ten[$gender][$i3]; # 10-19 | 1-9
+            // units without rub & kop
+            if ($uk>1) $out[]= morph($v,$unit[$uk][0],$unit[$uk][1],$unit[$uk][2]);
+        } //foreach
+    }
+    else $out[] = $nul;
+    $out[] = morph(intval($rub), $unit[1][0],$unit[1][1],$unit[1][2]); // rub
+    $out[] = $kop.' '.morph($kop,$unit[0][0],$unit[0][1],$unit[0][2]); // kop
+    return trim(preg_replace('/ {2,}/', ' ', join(' ',$out)));
+}
+
+/**
+ * Склоняем словоформу
+ * @ author AlexSoft
+ */
+function morph($n, $f1, $f2, $f5) {
+    $n = abs(intval($n)) % 100;
+    if ($n>10 && $n<20) return $f5;
+    $n = $n % 10;
+    if ($n>1 && $n<5) return $f2;
+    if ($n==1) return $f1;
+    return $f5;
+}
+
+function generate_random($length) {
+    $characters = '0123456789';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    return $randomString;
+}
+/*
+ * Вывод даты на русском языке [Функция используется в версии для печати]
+*/
+function rus_date() {
+// Перевод
+    $translate = array(
+        "am" => "дп",
+        "pm" => "пп",
+        "AM" => "ДП",
+        "PM" => "ПП",
+        "Monday" => "Понедельник",
+        "Mon" => "Пн",
+        "Tuesday" => "Вторник",
+        "Tue" => "Вт",
+        "Wednesday" => "Среда",
+        "Wed" => "Ср",
+        "Thursday" => "Четверг",
+        "Thu" => "Чт",
+        "Friday" => "Пятница",
+        "Fri" => "Пт",
+        "Saturday" => "Суббота",
+        "Sat" => "Сб",
+        "Sunday" => "Воскресенье",
+        "Sun" => "Вс",
+        "January" => "Января",
+        "Jan" => "Янв",
+        "February" => "Февраля",
+        "Feb" => "Фев",
+        "March" => "Марта",
+        "Mar" => "Мар",
+        "April" => "Апреля",
+        "Apr" => "Апр",
+        "May" => "Мая",
+        "June" => "Июня",
+        "Jun" => "Июн",
+        "July" => "Июля",
+        "Jul" => "Июл",
+        "August" => "Августа",
+        "Aug" => "Авг",
+        "September" => "Сентября",
+        "Sep" => "Сен",
+        "October" => "Октября",
+        "Oct" => "Окт",
+        "November" => "Ноября",
+        "Nov" => "Ноя",
+        "December" => "Декабря",
+        "Dec" => "Дек",
+        "st" => "ое",
+        "nd" => "ое",
+        "rd" => "е",
+        "th" => "ое"
+    );
+    // если передали дату, то переводим ее
+    if (func_num_args() > 1) {
+        $timestamp = func_get_arg(1);
+        return strtr(date(func_get_arg(0), $timestamp), $translate);
+    } else {
+// иначе текущую дату
+        return strtr(date(func_get_arg(0)), $translate);
+    }
+}
